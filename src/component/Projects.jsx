38dc39054ee9ce8f8c30aad3e12a9145a9d9210d
Keith <keithgpatino@gmail.com>
2026-04-04 17:04:45 +0800

    added projects wallah

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -1,8 +1,16 @@
 import ScrollFloat from './ScrollFloat';
+import FlowingMenu from './FlowingMenu';
 
+const demoItems = [
+    { link: '#', text: 'BAYANIHAN', image: 'https://picsum.photos/600/400?random=1' },
+    { link: '#', text: 'STUDYUS', image: 'https://picsum.photos/600/400?random=2' },
+    { link: '#', text: 'Monterey', image: 'https://picsum.photos/600/400?random=3' },
+    { link: '#', text: 'Monterey', image: 'https://picsum.photos/600/400?random=3' },
+    { link: '#', text: 'Sequoia', image: 'https://picsum.photos/600/400?random=4' }
+];
 function Projects() {
     return (
-        <div className="min-h-screen w-full relative bg-gray-900 flex items-center text-center flex-col gap-10 text-white p-52 ">
+        <div className="min-h-screen w-full relative bg-black flex items-center text-center flex-col gap-10 text-white p-52 ">
             <ScrollFloat
                 animationDuration={1}
                 ease='back.inOut(2)'
@@ -12,6 +20,16 @@ function Projects() {
             >
                 My Projects
             </ScrollFloat>
+            <div className="w-screen h-64 relative">
+                <FlowingMenu items={demoItems}
+                    speed={15}
+                    textColor="#ffffff"
+                    bgColor="#060010"
+                    marqueeBgColor="#ffffff"
+                    marqueeTextColor="#060010"
+                    borderColor="#ffffff"
+                />
+            </div>
         </div>
     );
 }
